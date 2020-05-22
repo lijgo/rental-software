1a3ae83b966659a8eee1204a616a0df0e82f0ec2
Levi <levi.johnson@student.hagley.school.nz>
2020-05-22 14:37:42 +1200

    Added basic car panel

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+	<title>Car Rental Software</title>
+	<link src="styles/index.css" />
+</head>
+<body>
+	<form>
+		<ul>
+			<li>
+				<input type="checkbox" id="ac" />
+				<label for="ac">
+					<img src="images/ac.png" height="128" width="128">blabla
+				</label>
+			</li>
+			<li>
+				<input type="checkbox" id="ac" />
+				<label for="ac">
+					<img src="images/ac.png" height="128" width="128">blabla
+				</label>
+			</li>
+			<li>
+				<input type="checkbox" id="ac" />
+				<label for="ac">
+					<img src="images/ac.png" height="128" width="128">blabla
+				</label>
+			</li>
+		</ul>
+	</form>
+</body>
+</html>
