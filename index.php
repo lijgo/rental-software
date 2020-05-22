c1b211fd464b36428a571aeb236e1cdbb614c830
Levi <levi.johnson@student.hagley.school.nz>
2020-05-22 14:53:07 +1200

    Added image checkboxes

--- index.php
+++ index.php
@@ -1,30 +1,33 @@
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Car Rental Software</title>
-	<link src="styles/index.css" />
+	<link rel="stylesheet" href="styles/index.css" />
 </head>
 <body>
 	<form>
 		<ul>
 			<li>
-				<input type="checkbox" id="ac" />
-				<label for="ac">
-					<img src="images/ac.png" height="128" width="128">blabla
+				<input type="checkbox" id="panelac" />
+				<label for="panelac">
+					<img src="images/ac.png" height="128" width="128">
 				</label>
 			</li>
 			<li>
-				<input type="checkbox" id="ac" />
-				<label for="ac">
-					<img src="images/ac.png" height="128" width="128">blabla
+				<input type="checkbox" id="panelradio" />
+				<label for="panelradio">
+					<img src="images/radio.jpg" height="128" width="128">
 				</label>
 			</li>
 			<li>
-				<input type="checkbox" id="ac" />
-				<label for="ac">
-					<img src="images/ac.png" height="128" width="128">blabla
+				<input type="checkbox" id="panelspare" />
+				<label for="panelspare">
+					<img src="images/spare.png" height="128" width="128">
 				</label>
 			</li>
 		</ul>
+
+		<input type="text" name="year" value="1998"> <input type="text" name="make" value="Honda"> <input type="text" name="model" value="Prelude"> <input type="text" name="year" value="1998">
 	</form>
 </body>
 </html>
